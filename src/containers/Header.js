2cd369c5047362b4b8a2b8c2930e48c50934fd68
Ashir <ashirq@outlook.com>
2020-07-17 09:15:08 +0100

    Initial commit. Basic container framework created for landing page.

---
+++ src/containers/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header() {
+
+  return (
+    <h1> This is the header container </h1>
+  )
+}
+
+export default Header;
---
+++ src/containers/LandingPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function LandingPage() {
+  return <p>This is the Landing page container!</p>
+}
+
+export default LandingPage;
